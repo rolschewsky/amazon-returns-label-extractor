04a2e76c83cf90adb7d19c73c49d0a506bc9860a
Rüdiger Alexander Olschewsky <ruediger.olschewsky@gmail.com>
2025-06-16 10:31:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,6 +7,8 @@
 // @match        https://www.amazon.de/spr/returns/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=amazon.de
 // @require      https://code.jquery.com/jquery-3.7.0.min.js
+// @updateURL    https://raw.githubusercontent.com/rolschewsky/amazon-returns-label-extractor/master/script.js
+// @downloadURL  https://raw.githubusercontent.com/rolschewsky/amazon-returns-label-extractor/master/script.js
 // @grant        none
 // ==/UserScript==
 const $ = window.$;
